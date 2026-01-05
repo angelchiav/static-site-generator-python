5ac8f80238ea8d11bc214f478bb54a239c94f91d
angelchiav <achiavicuna@gmail.com>
2026-01-05 16:12:30 -0300

    leaf node class

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -19,3 +19,21 @@ class HTMLNode:
     children = {self.children}
     props = {self.props}
 '''
+    
+class LeafNode(HTMLNode):
+    def __init__(self, tag=None, value=None, props=None):
+        super().__init__(tag=tag, value=value, children=None, props=props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("LeafNode must have a value")
+        
+        if self.tag is None:
+            return self.value
+        
+        props_html = ""
+        if self.props:
+            props_html = " " + self.props_to_html()
+
+        return f"<{self.tag}{props_html}>{self.value}</{self.tag}>"
+        
